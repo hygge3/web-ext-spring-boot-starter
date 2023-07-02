4463f8c940dace662c39dcb047bb80fe1b630dcc
hygge <zhanglh30@gmail.com>
2023-07-02 21:29:53 +0800

    ✨ 添加时区属性

--- src/main/java/ext/library/constant/HttpAttribute.java
+++ src/main/java/ext/library/constant/HttpAttribute.java
@@ -11,19 +11,22 @@ public interface HttpAttribute {
     /**
      * 链路 ID
      */
-    String TRACE_ID = "Trace-Id";
+    String TRACE_ID = Constant.PREFIX + "Trace-Id";
     /** 请求时间 */
-    String REQUEST_TIME = "Request-Time";
+    String REQUEST_TIME = Constant.PREFIX + "Request-Time";
+
+    /** 时区 */
+    String TIME_ZONE = Constant.PREFIX + "Time-Zone";
 
     // 请求属性
 
     /** 幂等性版本号请求 key */
-    String API_IDEMPOTENT_VERSION = "Api-Idempotent-Version";
+    String API_IDEMPOTENT_VERSION = Constant.PREFIX + "Api-Idempotent-Version";
 
     /**
      * ServletAsyncContext 阻塞超时时长 setAttribute 时所使用的固定变量名
      */
-    String SERVLET_ASYNC_CONTEXT_TIMEOUT_MILLIS = "Servlet-Async-Context-Timeout-Millis";
+    String SERVLET_ASYNC_CONTEXT_TIMEOUT_MILLIS = Constant.PREFIX + "Servlet-Async-Context-Timeout-Millis";
 
 
 }
