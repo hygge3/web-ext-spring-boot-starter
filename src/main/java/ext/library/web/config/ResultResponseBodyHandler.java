dbb2b256f4441f3b43dde3861da64639386d9a18
zlh <mrzhanglh@foxmail.com>
2023-08-07 11:24:48 +0800

    统一处理对 String 特殊处理

--- src/main/java/ext/library/web/config/ResultResponseBodyHandler.java
+++ src/main/java/ext/library/web/config/ResultResponseBodyHandler.java
@@ -1,5 +1,6 @@
 package ext.library.web.config;
 
+import com.alibaba.fastjson2.JSON;
 import ext.library.constant.HttpAttribute;
 import ext.library.util.I18nUtils;
 import ext.library.web.view.R;
@@ -32,13 +33,19 @@ public class ResultResponseBodyHandler implements ResponseBodyAdvice<Object> {
     }
 
     @Override
-    public Result<?> beforeBodyWrite(@Nullable Object body, @NonNull MethodParameter returnType, @NonNull MediaType selectedContentType, @NonNull Class<? extends HttpMessageConverter<?>> selectedConverterType, @NonNull ServerHttpRequest request, @NonNull ServerHttpResponse response) {
+    public Object beforeBodyWrite(@Nullable Object body, @NonNull MethodParameter returnType, @NonNull MediaType selectedContentType, @NonNull Class<? extends HttpMessageConverter<?>> selectedConverterType, @NonNull ServerHttpRequest request, @NonNull ServerHttpResponse response) {
         Result<?> result;
         // 1. 处理参数
         if (body == null) {
             result = R.success();
         } else if (body instanceof Result<?>) {
             result = (Result<?>) body;
+        } else if (body instanceof String) {
+            // String 特殊处理
+            result = R.success(body);
+            result.setMsg(I18nUtils.getExt(result.getMsg()));
+            result.setTraceId(MDC.get(HttpAttribute.TRACE_ID));
+            return JSON.toJSONString(result);
         } else {
             result = R.success(body);
         }
