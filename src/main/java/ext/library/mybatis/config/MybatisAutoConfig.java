72b0b73ac1f9f6c3c1062cb8102436e96e9652e4
zhanglh <mrzhanglh@foxmail.com>
2023-12-12 16:41:50 +0800

    🚧 一些配置调整

--- src/main/java/ext/library/mybatis/config/MybatisAutoConfig.java
+++ src/main/java/ext/library/mybatis/config/MybatisAutoConfig.java
@@ -14,6 +14,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Properties;
+
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
@@ -50,6 +52,13 @@ public class MybatisAutoConfig implements MyBatisFlexCustomizer {
     @Bean
     public PageInterceptor pageInterceptor() {
         log.info("【Mybatis-Flex】集成 PageHelper。执行初始化 ...");
+        PageInterceptor pageInterceptor = new PageInterceptor();
+        Properties properties = new Properties();
+        // 分页合理化参数，默认值为 false。当该参数设置为 true 时，pageNum<=0 时会查询第一页，pageNum>pages（超过总数时），会查询最后一页。默认 false 时，直接根据参数进行查询。
+        properties.setProperty("reasonable", "true");
+        // 支持通过 Mapper 接口参数来传递分页参数，默认值 false，分页插件会从查询方法的参数值中，自动根据上面 params 配置的字段中取值，查找到合适的值时就会自动分页。
+        properties.setProperty("supportMethodsArguments", "true");
+        pageInterceptor.setProperties(properties);
         return new PageInterceptor();
     }
 }
