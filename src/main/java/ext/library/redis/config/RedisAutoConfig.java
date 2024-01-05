e8a984ae9709934c4149ab01af7e576a953d2e24
zhanglh <mrzhanglh@foxmail.com>
2024-01-05 18:29:54 +0800

    feat(main): Redis 配置优化

--- src/main/java/ext/library/redis/config/RedisAutoConfig.java
+++ src/main/java/ext/library/redis/config/RedisAutoConfig.java
@@ -1,16 +1,15 @@
 package ext.library.redis.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ext.library.idempotent.ApiIdempotentController;
 import ext.library.redis.client.Redis;
-import ext.library.redis.config.properties.RedisProperties;
-import ext.library.redis.constant.RedisSerializerEnum;
+import ext.library.util.SpringUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -18,6 +17,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
@@ -28,10 +28,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @RequiredArgsConstructor
 @Import(ApiIdempotentController.class)
 @AutoConfigureAfter(RedisAutoConfiguration.class)
-@EnableConfigurationProperties({RedisProperties.class})
 public class RedisAutoConfig {
 
-    final RedisProperties redisProperties;
 
     /**
      * <p>支持 FastJson 进行 Redis 存储对象序列/反序列化
@@ -41,11 +39,9 @@ public class RedisAutoConfig {
     public RedisTemplate<String, Object> extRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-
         // 支持Jackson进行Redis存储对象序列/反序列化
-        if (redisProperties.getRedisSerializer() != RedisSerializerEnum.JDK) {
-            redisTemplate.setDefaultSerializer(redisProperties.getRedisSerializer().getRedisSerializer());
-        }
+        ObjectMapper objectMapper = SpringUtils.getBean(ObjectMapper.class);
+        redisTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
         redisTemplate.setKeySerializer(stringRedisSerializer);
         redisTemplate.setHashKeySerializer(stringRedisSerializer);
@@ -57,7 +53,7 @@ public class RedisAutoConfig {
     @Primary
     @ConditionalOnBean({RedisTemplate.class, StringRedisTemplate.class})
     public Redis redis(@Qualifier("extRedisTemplate") RedisTemplate<String, Object> redisTemplate, StringRedisTemplate stringRedisTemplate) {
-        log.info("【Redis】配置项：{}，Bean：Redis。执行初始化 ...", RedisProperties.PREFIX);
+        log.info("【Redis】Bean：Redis。执行初始化 ...");
         return new Redis(redisTemplate, stringRedisTemplate);
     }
 
--- src/main/java/ext/library/redis/config/properties/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * redis 自动配置属性
- */
-package ext.library.redis.config.properties;
